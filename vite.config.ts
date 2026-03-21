297b1b09cfe74064818141e5e97cc90ce1a8294b
peridotadvisory <peridotadvisory@gmail.com>
2026-03-21 14:09:14 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  assetsInclude: ['**/*.svg', '**/*.csv'], 
-}) 
-   
+  assetsInclude: ['**/*.svg', '**/*.csv'],
+})
